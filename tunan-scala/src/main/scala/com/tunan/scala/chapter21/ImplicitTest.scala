219b60f64ac624c0ad4e19af00b69e01ead30cd9
yerias <1748094638@qq.com>
2020-12-02 17:27:19 +0800

    2020-12-02 commit

---
+++ tunan-scala/src/main/scala/com/tunan/scala/chapter21/ImplicitTest.scala
@@ -0,0 +1,22 @@
+package com.tunan.scala.chapter21
+
+object ImplicitTest {
+
+    def main(args: Array[String]): Unit = {
+
+        implicit def int2Double(x:Int):Double = x.toDouble
+
+        implicit def double2Int(x:Double):Int = x.toInt
+
+        implicit class RectangleMaker(width:Int){
+            def x(height:Int): Rectangle = Rectangle(width,height)
+        }
+
+
+
+    }
+}
+
+case class Rectangle(width:Int,height:Int)
+
+
