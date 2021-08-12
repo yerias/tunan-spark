a20a6e0723d2fd1433a7a2af63a7281afb3e30bc
李沅芮 <971118017@qq.com>
2021-08-12 08:15:19 +0800

    20210812

--- tunan-spark-sql/src/main/scala/com/tunan/spark/sql/mongo/DefaultSource.scala
+++ tunan-spark-sql/src/main/scala/com/tunan/spark/sql/mongo/DefaultSource.scala
@@ -3,12 +3,23 @@ package com.tunan.spark.sql.mongo
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.sources.{BaseRelation, RelationProvider}
 
-class DefaultSource extends RelationProvider{
+class DefaultSource extends RelationProvider {
     override def createRelation(sqlContext: SQLContext, parameters: Map[String, String]): BaseRelation = {
 
-        val start = parameters.get("start").get.toLong
-        val end = parameters.getOrElse("end", start + 86400000L).toString.toLong
+        val fields = parameters.get("fields")
+        val start = parameters.get("start")
+        if (fields.isEmpty) {
+            throw new IllegalArgumentException("缺少查询的字段")
+        }
 
-        new MongoDataSourceRelation(sqlContext,start,end)
+        if (start.isEmpty) {
+            throw new IllegalArgumentException("缺少开始的时间")
+        }
+
+
+        val end = parameters.getOrElse("end", start.get.toLong + 86400000L).toString.toLong
+
+        new MongoDataSourceRelation(sqlContext, start.get.toLong, end, fields.get)
+
     }
 }
